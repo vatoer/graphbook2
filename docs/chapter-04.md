06a6cbce0bba70cb7d8466037cbcd2867c7980ee
Fathur Rohman <fathur.rohman@kemlu.go.id>
2023-04-10 10:07:49 +0200

    Chapter 4, Hooking Apollo into React - installing apollo client

--- docs/chapter-04.md
+++ docs/chapter-04.md
@@ -7,3 +7,18 @@
 - Chapter 9, Implementing Server-Side Rendering
 - Chapter 10, Real-Time Subscriptions
 - Chapter 11, Writing Tests for React and Node.js
+
+# Chapter 4, Hooking Apollo into React
+-  Installing and configuring Apollo Client
+-  Sending requests with GQL and Apollo's Query component
+-  Mutating data with Apollo Client
+-  Implementing chats and messages
+-  Pagination in React and GraphQL
+-  Debugging with the Apollo Client Devtools
+
+## installing apollo client 
+```sh
+npm install --save @apollo/client graphql
+mkdir src/client/apollo
+touch src/client/apollo/index.js
+```
---
+++ src/client/apollo/index.js
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, from, HttpLink, ApolloLink } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const client = new ApolloClient({
+    link: from([
+        onError(({ graphQLErrors, networkError }) => {
+            if(graphQLErrors) {
+                graphQLErrors.map(({ message, locations, path }) => {
+                    console.log(`[GraphQL Error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+                });
+                if (networkError) {
+                    console.log(`[Network eror]: ${networkError}`)
+                }
+            }
+        }),
+        new HttpLink({
+            uri: 'http://127.0.0.1:8000/graphql'
+        }),
+    ]),
+    cache: new InMemoryCache(),
+});
+
+export default client;
